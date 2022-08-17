875d9eeefda7253d1bf0443c7c4def6ad8be2092
kartik18g <gupta.kartik18kg@gmail.com>
2022-08-17 18:27:10 +0530

    change

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ const PORT = process.env.PORT || 8080
 app.get('/',(req,res)=>res.send(`Server is running on port ${PORT}`))
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT} !`)
+    console.log(`Server is running on port ${PORT} `)
 })
 
